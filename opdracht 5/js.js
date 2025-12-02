3ad500edc54c124993f367ae65ff9fc218e88300
Lucas-j-d-f-c <9028157@student.zadkine.nl>
2025-12-02 10:08:10 +0100

    gewerkt aan 5 en 6 af denk ik

---
+++ opdracht 5/js.js
@@ -0,0 +1,4 @@
+let getal = 10;
+let tekst ="20"; 
+string + 10;
+Document
---
+++ opdracht 6/js.js
@@ -0,0 +1,7 @@
+
+function kleur_aanpassen() { 
+document.getElementById("titel").innerHTML ="Hoi"
+document.getElementById("titel").style.color="red";
+}
+
+
