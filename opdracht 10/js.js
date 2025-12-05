0b85d8b35f6bd8d2358817f4e6b35e7a10b177f6
Lucas-j-d-f-c <9028157@student.zadkine.nl>
2025-12-05 11:04:39 +0100

    opdracht 10 af

---
+++ opdracht 10/js.js
@@ -0,0 +1,13 @@
+function groet_ochtend(){
+document.getElementById("title").innerHTML =("Goedemorgen!");
+document.getElementById("title").style.backgroundColor =("yellow");
+}
+function groet_middag(){
+document.getElementById("title").innerHTML =("Goedemiddag!");
+document.getElementById("title").style.backgroundColor =("orange");
+}
+function groet_avond(){
+document.getElementById("title").innerHTML =("Goedenavond!");
+document.getElementById("title").style.backgroundColor =("darkblue")
+document.body.style.color = "white"
+}
