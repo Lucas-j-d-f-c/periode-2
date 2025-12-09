7a37ee617c26ea3f3efaad081df5e434a963fc04
Lucas-j-d-f-c <9028157@student.zadkine.nl>
2025-12-09 09:12:46 +0100

    hrughsrugh

--- hoofdopdracht/js.js
+++ hoofdopdracht/js.js
@@ -1,7 +1,11 @@
 function dark_mode(){
-document.getElementById.innerHTML =("good night 🌙")
-doc
+document.getElementById("title").innerHTML =("good night 🌙")
+document.body.style.backgroundColor = "black";
+document.body.style.color ="orange"
+
 }
 function light_mode(){
-document.getElementById.innerHTML =("good morning☀️")
+document.getElementById("title").innerHTML =("good morning☀️")
+document.body.style.backgroundColor = "white";
+document.body.style.color ="black"
 }
