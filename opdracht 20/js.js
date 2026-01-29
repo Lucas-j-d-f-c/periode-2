0e382491c12370bea7115d7781e2e5c239cf5e73
Lucas-j-d-f-c <9028157@student.zadkine.nl>
2026-01-29 09:56:25 +0100

    fortnite 67 ohio

--- opdracht 20/js.js
+++ opdracht 20/js.js
@@ -1,1 +1,8 @@
 let emojis = ["🍆" , "🍑" , "🍌" , "🍒" , "🍓"]
+
+const button = document.getElementById("goon");
+const h1 = document.querySelector("h1");
+button.addEventListener("click", function() {
+    let randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+    h1.textContent += randomEmoji;
+});
